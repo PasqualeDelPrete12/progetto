6edbd4f4190da923697dee21128dd7f4e5303720
pdelprete <pasquale.delprete@3fedin.it>
2025-06-12 15:54:16 +0200

    Aggiungi prodotto model mapper

--- demo/src/main/java/com/example/demo/services/ServiziProdotto.java
+++ demo/src/main/java/com/example/demo/services/ServiziProdotto.java
@@ -1,11 +1,12 @@
 package com.example.demo.services;
 
+import com.example.demo.controller.request.ProdottoRequest;
 import com.example.demo.entity.ProdottoEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ServiziProdotto {
-    void aggiungiProdotto(com.example.demo.entity.ProdottoEntity prodotto);
+    void aggiungiProdotto(ProdottoRequest prodotto);
 
      ProdottoEntity modificaProdotto(ProdottoEntity prodotto, Long idProdotto);
 }
--- demo/src/main/java/com/example/demo/services/ServiziProdottoImpl.java
+++ demo/src/main/java/com/example/demo/services/ServiziProdottoImpl.java
@@ -1,23 +1,27 @@
 package com.example.demo.services;
 
+import com.example.demo.controller.request.ProdottoRequest;
 import com.example.demo.entity.ProdottoEntity;
 import com.example.demo.repository.ProdottoRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 public class ServiziProdottoImpl implements ServiziProdotto {
+    ModelMapper modelMapper;
 
-    @Autowired // Inject your ProdottoRepository
+    @Autowired
     private ProdottoRepository prodottoRepository;
 
     /**
-     * @param prodotto
+     * @param request
      */
     @Override
-    public void aggiungiProdotto(ProdottoEntity prodotto) {
-
+    public void aggiungiProdotto(ProdottoRequest request) {
+        ProdottoEntity entity = modelMapper.map(request, ProdottoEntity.class);
+        prodottoRepository.save(entity);
     }
 
     /**
@@ -35,7 +39,7 @@ public class ServiziProdottoImpl implements ServiziProdotto {
                             prodottoEsistente.setPrezzo(prodotto.getPrezzo());
                                 prodottoEsistente.setDisponibile(prodotto.isDisponibile());
             }else{
-                throw new RuntimeException("Il prodotto"+prodotto+"non è stato modificato.");
+                throw new RuntimeException(STR."Il prodotto\{prodotto}non è stato modificato.");
             }
         return null;
     }
