74085aea8fa3c91560a00906848317f84eb7eb80
domenico.ponticelli <domenico.ponticelli@consultant.rgigroup.com>
2025-06-12 14:53:11 +0200

    modificaProdottoService

--- demo/src/main/java/com/example/demo/services/ServiziProdottoImpl.java
+++ demo/src/main/java/com/example/demo/services/ServiziProdottoImpl.java
@@ -1,11 +1,17 @@
 package com.example.demo.services;
 
 import com.example.demo.entity.ProdottoEntity;
+import com.example.demo.repository.ProdottoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ServiziProdottoImpl implements ServiziProdotto {
 
+    @Autowired // Inject your ProdottoRepository
+    private ProdottoRepository prodottoRepository;
 
     /**
      * @param prodotto
@@ -22,6 +28,16 @@ public class ServiziProdottoImpl implements ServiziProdotto {
      */
     @Override
     public ProdottoEntity modificaProdotto(ProdottoEntity prodotto, Long idProdotto) {
+        Optional<ProdottoEntity> prodottoEsistenteOptional = prodottoRepository.findById(idProdotto);
+            if (prodottoEsistenteOptional.isPresent()){
+                ProdottoEntity prodottoEsistente = prodottoEsistenteOptional.get();
+                    prodottoEsistente.setNome(prodotto.getNome());
+                        prodottoEsistente.setDescrizione(prodotto.getDescrizione());
+                            prodottoEsistente.setPrezzo(prodotto.getPrezzo());
+                                prodottoEsistente.setDisponibile(prodotto.isDisponibile());
+            }else{
+                throw new RuntimeException("Il prodotto"+prodotto+"non è stato modificato.");
+            }
         return null;
     }
 }
